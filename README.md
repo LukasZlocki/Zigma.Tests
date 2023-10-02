baf5e1d4032ac653c1cffb60cd491f71b6178035
lzlocki <lzlocki@poczta.fm>
2023-10-02 21:08:20 +0200

    Create project and add first test.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Zigma.Tests
+
+Set of xunit tests for Zigma Tool.
+Link to project:
+https://github.com/LukasZlocki/Zigma
+
+
+## Technologies Used
+
+* C#
+* .Net
+* xunit
+
+
+## Creator
+
+Created by Lukas Zlocki  
---
+++ StructureTransformTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using Zigma.Models;
+using Zigma.TransformationTools;
+using System.ComponentModel;
+
+namespace Zigma.Tests
+{
+    public class StructureTransformTests
+    {
+        [Fact]
+        public void ColumnRemove_ShouldRemoveGivenColumn()
+        {
+            // Arrange
+            List<string[]> TestSet = new List<string[]>{
+                new string[] {"aa", "bb", "cc", "dd"},
+                new string[] {"ee", "ff", "gg", "hh"},
+                new string[] {"ii", "jj", "kk", "ll"}
+            };
+
+            List<string[]> ExpectedTestSet = new List<string[]>{
+                new string[] {"aa", "cc", "dd"},
+                new string[] {"ee", "gg", "hh"},
+                new string[] {"ii", "kk", "ll"}
+            }; 
+
+            ZigmaModel expectedModel = new();
+            expectedModel.CreateZigmaDatasetFromRawDataset(ExpectedTestSet);
+
+            // Act
+            ZigmaModel actualModel = new();
+            actualModel.CreateZigmaDatasetFromRawDataset(TestSet);
+            TransformationTool transform = new();
+            actualModel = transform.ColumnRemove(actualModel, 1);
+
+            // Assert
+            Assert.Equal(expectedModel.GetRawZigmaDataset(), actualModel.GetRawZigmaDataset());
+
+        }
+    }
+}
